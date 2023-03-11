e5b56dc8e0d0a7cd07d4fb49f3d488d53208cb65
Bvsty <alvarezbastian7@gmail.com>
2023-03-11 04:31:41 -0300

    asd

---
+++ Tarea/gotti.py
@@ -0,0 +1,10 @@
+password="GOTTITEAMO"
+print("---------------------------------------------------")
+while(True):
+    Contra=input(str("Introduzca la contraseña: "))
+
+    if Contra==password:
+        print("acceso Correcto.")
+    
+    else:
+        print("Contraseña Incorrecta")
